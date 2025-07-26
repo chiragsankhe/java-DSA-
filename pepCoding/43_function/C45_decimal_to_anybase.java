97af9db0623b925582f20dbcc947cc7907ca6a5d
chirag sankhe <chiragsankhe692001@gmail.com>
2025-07-26 14:48:33 +0530

    decimal_anybase

--- pepCoding/43_function/C45_decimal_to_anybase.java
+++ pepCoding/43_function/C45_decimal_to_anybase.java
@@ -2,6 +2,26 @@ import java.util.*;
 
 public class C45_decimal_to_anybase
 {
+    public static int getValueInBase(int n , int b )
+    {
+        int rv = 0 ;
+
+        int p = 1;
+
+        while(n>0)
+        {
+            int digit = n%b;
+            n = n/b;
+
+            rv += digit * p;
+
+            p = p * 10;
+        }
+
+
+        return rv ;
+    }
+
     public static void main(String args[])
     {
         Scanner scn = new Scanner(System.in);
@@ -13,22 +33,4 @@ public class C45_decimal_to_anybase
 
     }
 
-    public static int getValueInBase( int n , int b)
-    {
-        int rv = 0;
-
-
-        int p = 1 ;
-
-        while(n > 0)
-        {
-            int dig = n % b;
-            n = n/b;
-
-            rv += dig * p;
-            p = p *10;
-
-        }
-        return rv;
-    }
-}    
+}
