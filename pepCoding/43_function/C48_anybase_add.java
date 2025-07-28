96842216d767c9a3c48579bd60bddc40522e2e79
chirag sankhe <chiragsankhe692001@gmail.com>
2025-07-29 03:32:26 +0530

    update file

--- pepCoding/43_function/C48_anybase_add.java
+++ pepCoding/43_function/C48_anybase_add.java
@@ -2,6 +2,33 @@ import java.util.*;
 
 public class C48_anybase_add
 {
+   public static int getSum(int b , int n1 , int n2)
+    {
+        int rv = 0;
+
+        int c = 0 ;
+        int p = 1 ;
+
+        while(n1>0 || n2>0 || c>0)
+        {
+            int n1_val = n1%10;
+            int n2_val = n2%10;
+
+            n1 = n1/10;
+            n2 = n2/10;
+
+            int d = n1_val  + n2_val + c;
+
+            c = d/b;
+            d = d%b;
+
+            rv += d * p ;
+            p = p*10; 
+        }
+        return rv;
+    }
+
+
     public static void main(String args[])
     {
         Scanner scn = new Scanner(System.in);
@@ -13,32 +40,23 @@ public class C48_anybase_add
         int d = getSum(b, n1, n2);
         System.out.println(d);
     }
-
-    public static int getSum(int b , int n1 , int n2)
-    {
-        int rv = 0;
-         
-         int c = 0;
-         int p = 1;
-         while(n1 > 0 || n2 > 0 || c > 0 )
-         {
-            int d1 = n1 % 10;
-            int d2 = n2 % 10;
-            n1 = n1 / 10;
-            n2 = n2 /10;
-
-            int d = d1 + d2 + c ;
-            c = d/ 8;
-            d = d % b;
-
-
-            rv +=  d * p;
-            p = p * 10;
-         }
-
-
-
-        return rv ;
-
-    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
