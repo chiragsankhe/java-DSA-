2e6b7b0c8dcdf35307e6160f0edf40a6c5bcd79d
chirag sankhe <chiragsankhe692001@gmail.com>
2025-07-29 04:20:16 +0530

    arrat 52 53 54 done

--- pepCoding/51_array/C52_array.java
+++ pepCoding/51_array/C52_array.java
@@ -3,42 +3,83 @@ import java.io.*;
 
 public class C52_array
 {
-    public static void main(String args[]) throws Exception
+    static int min_max_diff(int arr[])
     {
-        Scanner scn = new Scanner(System.in);
-      
-        int n = scn.nextInt();
-         
-          int[] arr = new int[n];
+        
 
-           for(int i = 0; i < arr.length ; i++)
-           {
-            arr[i] = scn.nextInt();
+        int min = arr[0];
+        int max = arr[0];
 
-           }
+        
 
-
-           int max = arr[0];
-           int min = arr[0];
-           for(int i = 1; i < arr.length ; i++)
-           {
-            if(arr[i] > max)
+        for(int i = 1 ;i<arr.length;i++)
+        {
+            if(arr[i] < min)
+            {
+                min = arr[i];
+            }
+            else if(arr[i] > max)
             {
                 max = arr[i];
             }
+        }
+        int diff= max - min ;
 
-            if (arr[i] < min )
-            {
-               min = arr[i] ; 
-            }
+        return diff;
+    }
+
+    public static void main(String args[]) throws Exception
+    {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        
+        int arr[] = new int[n];
+
+        for(int i = 0 ;i<n;i++)
+        {
+            arr[i] = scn.nextInt();
+        }
 
 
-           }
-
-          int   span = max - min ;
-           System.out.println(span);
-
-
+        int result = min_max_diff(arr);
+        System.out.println(result);
+      
+       
        
     }
 }
+
+
+
+//  int n = scn.nextInt();
+         
+//           int[] arr = new int[n];
+
+//            for(int i = 0; i < arr.length ; i++)
+//            {
+//             arr[i] = scn.nextInt();
+
+//            }
+
+
+//            int max = arr[0];
+//            int min = arr[0];
+//            for(int i = 1; i < arr.length ; i++)
+//            {
+//             if(arr[i] > max)
+//             {
+//                 max = arr[i];
+//             }
+
+//             if (arr[i] < min )
+//             {
+//                min = arr[i] ; 
+//             }
+
+
+//            }
+
+//           int   span = max - min ;
+//            System.out.println(span);
+
--- pepCoding/51_array/C54_bar_chart.java
+++ pepCoding/51_array/C54_bar_chart.java
@@ -6,47 +6,98 @@ public class C54_bar_chart
     public static void main(String args[])
     {
         Scanner scn = new Scanner(System.in);
+
         int n = scn.nextInt();
 
-        int [] arr = new int[n];
-
-        for(int i = 0; i < arr.length ; i++)
+        int arr[] = new int[n];
+        for(int i = 0 ;i<n;i++)
         {
             arr[i] = scn.nextInt();
+
         }
 
-        int max = arr[0];
-        for(int i = 1 ; i < arr.length ; i++)
+        int max = 0 ;
+        for(int i = 0 ;i<n;i++)
         {
-            if( arr[i] > max)
+            if(arr[i]> max)
             {
                 max = arr[i];
             }
-            
         }
 
-        for(int floar = max; floar >= 1 ; floar-- )
+
+        for(int i = max;i>0;i--)
         {
-            for(int i = 0 ; i < arr.length ; i++)
+            for(int j = 0 ;j<n;j++)
             {
-                if(arr[i] > floar)
+                if(arr[j] >=i)
                 {
                     System.out.print("*\t");
-                }else
-            {
-                System.out.print("\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
             }
+            System.out.println();
+        }
+    }
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  {
+//         Scanner scn = new Scanner(System.in);
+//         int n = scn.nextInt();
+
+//         int [] arr = new int[n];
+
+//         for(int i = 0; i < arr.length ; i++)
+//         {
+//             arr[i] = scn.nextInt();
+//         }
+
+//         int max = arr[0];
+//         for(int i = 1 ; i < arr.length ; i++)
+//         {
+//             if( arr[i] > max)
+//             {
+//                 max = arr[i];
+//             }
+            
+//         }
+
+//         for(int floar = max; floar >= 1 ; floar-- )
+//         {
+//             for(int i = 0 ; i < arr.length ; i++)
+//             {
+//                 if(arr[i] > floar)
+//                 {
+//                     System.out.print("*\t");
+//                 }else
+//             {
+//                 System.out.print("\t");
+//             }
 
                 
-            }
-              System.out.println();
+//             }
+//               System.out.println();
            
-        }
+//         }
         
 
         
 
 
 
-    }
-}
+//     }
