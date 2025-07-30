21859dca766ba230e0b7167f4397707805334320
chirag sankhe <chiragsankhe692001@gmail.com>
2025-07-31 00:01:46 +0530

    balance_bracket

--- pepCoding/51_array/C61_blanced_bracket.java
+++ pepCoding/51_array/C61_blanced_bracket.java
@@ -1,74 +1,56 @@
 import java.util.*;
-import java.io.*;
 
-public class C61_balanced_bracket
-{
-    public static void main(String args[])
-    {
-        Scanner scn = new Scanner(System.in());
+public class C61_blanced_bracket {
+
+    static boolean handleClosing(Stack<Character> st, char correspondingOpen) {
+        if (st.size() == 0) {
+            return false;
+        } else if (st.peek() != correspondingOpen) {
+            return false;
+        } else {
+            st.pop();
+            return true;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
         String str = scn.nextLine();
 
         Stack<Character> st = new Stack<>();
-         for(int i = 0 ; i < str.length() ; i++){
-            char ch = scn.charAt(i);
-            if(ch == '(' || ch == '{' || ch == '['){
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == '(' || ch == '[' || ch == '{') {
                 st.push(ch);
-
-            }else if(ch == ')'){
-              boolean val = handleCloseing(st, '(');
-              if(val == false){
-                System.out.println(val);
-                return;
-              }
-
-
+            } else if (ch == ')') {
+                boolean val = handleClosing(st, '(');
+                if (val == false) {
+                    System.out.println(false);
+                    return;
+                }
+            } else if (ch == '}') {
+                boolean val = handleClosing(st, '{');
+                if (val == false) {
+                    System.out.println(false);
+                    return;
+                }
+            } else if (ch == ']') {
+                boolean val = handleClosing(st, '[');
+                if (val == false) {
+                    System.out.println(false);
+                    return;
+                }
+            } else {
+                // ignore characters that are not brackets
             }
-            else if(ch == '}'){
-                boolean val = handleCloseing(st, '{');
-              if(val == false){
-                System.out.println(val);
-                return;
-              }
-            }
-            else if(ch == ']'){
-               boolean val = handleCloseing(st, '[');
-              if(val == false){
-                System.out.println(val);
-                return;
-              }
-            } else{
-
-            }
-        } 
-
-        if(st.size() == 0){
-             System.out.println(true);
         }
-        }
-        else{
-             System.out.println(false);
-        }
-        
 
-
+        if (st.size() == 0) {
+            System.out.println(true);  // All brackets matched
+        } else {
+            System.out.println(false); // Some unmatched opening brackets
+        }
     }
-
-
-    public static void handleCloseing( Stack<character> st, char corresoch){
-         if(st.size() == 0)
-         {
-            return false;
-         }
-         else if( st.peek() != corresoch){
-            return false;
-
-         }else{
-            st.pop();
-            return true;
-         }
-    }
-
-
-
-
-
+}
