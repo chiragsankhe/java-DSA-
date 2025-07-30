e50f5029639ce8e46d83259587bd9ed870f3d455
chirag sankhe <chiragsankhe692001@gmail.com>
2025-07-30 23:03:28 +0530

    dublicate_bracket

--- pepCoding/51_array/C60_dublicate_bracket.java
+++ pepCoding/51_array/C60_dublicate_bracket.java
@@ -8,27 +8,42 @@ public class C60_dublicate_bracket
         Scanner scn = new Scanner(System.in);
         String str = scn.nextLine();
 
-        Stack<Character> st = new Stack<>();
-        for(int i = 0; i < str.length() ; i++){
-            char ch = str.charAt(i);
-            if(ch == ')'){
-                if(st.peek() == '(' ){
-                    System.out.println(true);
-                    return;
-                }else{
-                    while(st.peek() != '('){
-                        st.pop();
-                    }
-                    st.pop();
-                }
-            }
-            else{
-                st.push(ch);
+       Stack<Character> st = new Stack<>();
 
+       for(int i = 0 ;i<str.length();i++)
+       {
+        char ch = str.charAt(i);
+        if(ch == ')')
+        {
+           if(st.peek() == '(')
+           {
+            System.out.println("true");
+            return;
+           }
+           else{
+            while(st.peek() !='(')
+            {
+                st.pop();
             }
+            st.pop();
+           }
         }
-        System.out.println(false);
-    
+        else
+        {
+            st.push(ch);
+        }
+ 
+       }
+      
 
+      if (st.isEmpty()) {
+          System.out.println(false);
+         } else {
+            System.out.println(true);
+           }
+
+
+
+     
     }
 }
