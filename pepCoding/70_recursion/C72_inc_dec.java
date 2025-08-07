6ed9fde4b2926d7c655493eafd52dd0a9ecd8ef5
chirag sankhe <chiragsankhe692001@gmail.com>
2025-08-07 17:08:27 +0530

    increasing and decreasing

--- pepCoding/70_recursion/C72_inc_dec.java
+++ pepCoding/70_recursion/C72_inc_dec.java
@@ -1,13 +1,14 @@
 import java.util.*;
 
- public class C71_dec
+ public class C72_inc_dec
 {
-     static void print_decresing(int n )
+     static void print_inc_dec(int n )
     {
         if(n == 0) return;
 
         System.out.print(n + "\t");
-        print_decresing(n-1);
+        print_inc_dec(n-1);
+        System.out.print(n + "\t");
     }
 
     public static void main(String args[])
@@ -16,6 +17,6 @@ import java.util.*;
 
         int n = scn.nextInt();
 
-        print_decresing(n);
+        print_inc_dec(n);
     }
 }
