76c05847668dc8da9c02dfc8321cd843e0b9e636
chirag sankhe <chiragsankhe692001@gmail.com>
2025-09-01 11:18:56 +0530

    quicksort

---
+++ C5_quicksort.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class C5_quicksort
+{ public static int  partition(int arr[],int low ,int high){
+    int pivot = arr[high];
+    int i = low -1;
+    for(int j = low;j<high;j++){
+        if(arr[j]<pivot){
+            i++;
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+    }
+
+    // swap pivot
+    int temp = arr[i+1];
+    arr[i+1] = arr[high];
+    arr[high] = temp;
+
+    return i+1;
+
+}
+    public static void quicksort(int arr[],int low ,int high){
+        if(low<high){
+            int pi = partition(arr,low ,high);
+
+            quicksort(arr,low,pi-1);
+            quicksort(arr,pi+1,high);
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0 ;i<n;i++)
+        {
+            arr[i] = scn.nextInt();
+        }
+
+        quicksort(arr,0,arr.length-1);
+        for(int i = 0 ;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
